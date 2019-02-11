2c675477a1ee5198c26836defda2bbe3f33427a0
moxDroid Labs Inc <pritam.world@gmail.com>
2019-02-11 18:56:39 -0500

    Function deflation and calling with label

--- SwiftDeclaringAndUsingFunctions/main.swift
+++ SwiftDeclaringAndUsingFunctions/main.swift
@@ -8,5 +8,35 @@
 
 import Foundation
 
-print("Hello, World!")
+func hello() {
+    print("Hello")
+}
 
+hello()
+
+func sum(a: Int, b: Int)
+{
+    let c = a + b
+    print("SUM - I : \(c)")
+}
+
+//With Default Lable
+sum(a: 10, b: 20)
+
+func sum(of a: Float, and b: Float)
+{
+    let c = a + b
+    print("SUM - F : \(c)")
+}
+
+//With user defined lable
+sum(of: 10.5, and: 20.0)
+
+func sub(_ a: Int, _ b: Int)
+{
+    let c = a - b
+    print("SUB - I : \(c)")
+}
+
+//With not lable
+sub(10, 5)
