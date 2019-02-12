a95605641883b8b3a1a57b16bb6c6fd82c4a731f
moxDroid Labs Inc <pritam.world@gmail.com>
2019-02-11 19:12:36 -0500

    Default Values functions added

--- SwiftDeclaringAndUsingFunctions/main.swift
+++ SwiftDeclaringAndUsingFunctions/main.swift
@@ -40,3 +40,42 @@ func sub(_ a: Int, _ b: Int)
 
 //With not lable
 sub(10, 5)
+
+//---------------------------
+
+func greet(name: String) -> String
+{
+    let s = "Welcome, \(name)"
+    return s
+}
+
+let s = greet(name: "Pritesh")
+print(s)
+
+//Return Tuples
+func exchange(a: Int, b: Int) -> (Int, Int)
+{
+    return (b,a)
+}
+
+var x = 100
+var y = 300
+print(x,y)
+
+(x,y) = exchange(a: x, b: y)
+
+print(x,y)
+
+//Default Values
+func si(amount: Float = 100, rate: Float, noOfYear: Float = 2) -> Float
+{
+    return (amount * rate * noOfYear) / 100.0
+}
+
+print(si(amount: 1000, rate: 2.5, noOfYear: 10))
+print(si(amount: 1000, rate: 2.5))
+print(si(rate: 2.5))
+
+
+
+
